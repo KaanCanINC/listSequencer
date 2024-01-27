bb318e5c776f76e9512a56d770f60e5e87c945a7
KaanCanINC <kaan.yazilim@outlook.com>
2024-01-27 18:53:24 +0300

    COMPLETED

--- src/components/Textareas.jsx
+++ src/components/Textareas.jsx
@@ -1,29 +1,27 @@
+// const Textareas = (disabled) => {
 
+//     return (
+//         <>
+//             <textarea
+//                 value={inputText}
+//                 className='sorter__content-textarea'
+//                 onChange={handleInputChange}
+//                 placeholder="Metin girin..."
+//                 cols='50'
+//                 rows='40'
+//             ></textarea>
+//             <textarea
+//                 value={outputText}
+//                 readOnly
+//                 className='sorter__content-textarea'
+//                 placeholder="Listenizin son hali burada görüntülenecek."
+//                 cols='50'
+//                 rows='40'
+//             ></textarea>
+//         </>
 
+//     );
+// };
 
-const Textareas = (disabled) => {
-
-    return (
-        <>
-            <textarea
-                value={inputText}
-                className='sorter__content-textarea'
-                onChange={handleInputChange}
-                placeholder="Metin girin..."
-                cols='50'
-                rows='40'
-            ></textarea>
-            <textarea
-                value={outputText}
-                readOnly
-                className='sorter__content-textarea'
-                placeholder="Listenizin son hali burada görüntülenecek."
-                cols='50'
-                rows='40'
-            ></textarea>
-        </>
-
-    );
-};
-
-export default Sorter;
+// export default Sorter;
+//yeni sayfalar eklenebilir bu durumda textareaları component olarak yazmam gerek
